eb96ef2584194c8b0e3b91f1a61a18bb864f8cd0
ow-tech <wagwanwekon@outloo.com>
2020-02-21 12:41:45 +0300

    constructing of jquery functions for what-we-do

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,31 @@
 $(document).ready(function(){
     $("img.design ").click(function(){
-        $("p#design").toggle();
+        $("p#design").slideToggle();
+        $("img.design").slideToggle();
+    })
+    $("p.des").click(function(){
+        $("img.design").slideToggle();
+        $("p#design").slideToggle();
+    });
+
+    $("img.development").click(function(){
+        $("p#development").slideToggle();
+        $("img.development").slideToggle();
+    })
+    $("p.dev").click(function(){
+        $("img.development").slideToggle();
+        $("p#development").slideToggle();
+    });
+
+    $("img.product").click(function(){
+        $("p#product").slideToggle();
+        $("img.product").slideToggle();
+    })
+    $("p.pro").click(function(){
+        $("img.product").slideToggle();
+        $("p#product").slideToggle();
 
     })
+
+
 })
